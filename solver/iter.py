a4aa5cea1033a7acebc3b4d778190a57acf7cdb2
Jack Hales <halesyy@gmail.com>
2021-12-19 11:23:31 +1100

    algo

--- solver/iter.py
+++ solver/iter.py
@@ -2,3 +2,12 @@
 import json
 
 primes = json.loads(open("../datasets/primes.json", "r").read())
+
+# Conceptions:
+# - Are primes a graphical pattern?
+# - If they follow a pattern, where is it?
+# - If they don't follow a pattern, there's no chance.
+# - Miller-Rabin primariliy test. Fermat primality test. See what tools they use.
+# - Diff between:
+#   1. Predicting the next prime.
+#   2. Classifying a number as a prime.
