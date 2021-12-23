eb123ce129127e080986fdc7f17807a708410a78
Jack Hales <halesyy@gmail.com>
2021-12-24 08:54:34 +1100

    move

--- solver/iter.py
+++ solver/iter.py
@@ -1,5 +1,7 @@
 
 import json
+from py_expression_eval import Parser
+from random import randint
 
 primes = json.loads(open("../datasets/primes.json", "r").read())
 
@@ -21,3 +23,34 @@ primes = json.loads(open("../datasets/primes.json", "r").read())
 # - fx = x first.
 # - fx = 2x second.
 # - fx = next prime. (lol)
+
+meta = {
+    "glue": [
+        "+",
+        "-",
+        "*",
+        "/",
+        "^",
+        "%"
+    ],
+    "attachments": [
+        "sin(x)",
+        "cos(x)",
+        "tan(x)",
+        "log(x, y)",
+        "log(x)",
+        "e",
+        "pi"
+    ]
+}
+
+mp = Parser()
+eval = lambda ex, sub={}: mp.parse(ex).evaluate(sub)
+
+v1 = eval("5*2*x", {"x": 5})
+
+def make_eq():
+    # make line of aaagaaaga where
+    # g can only be between a, like DNA
+    # can we have a 4-type? brackets?
+    length = randint(1, 100)
