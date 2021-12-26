eecc2250e945c5fcf7e36fd36f29255868374224
Jack Hales <halesyy@gmail.com>
2021-12-26 15:06:41 +1100

    great

--- datasets/diff.py
+++ datasets/diff.py
@@ -1,6 +1,6 @@
 import json
 import matplotlib.pyplot as plt
-primes = json.loads(open("primes.json", "r").read())
+primes = json.loads(open("primes_diff_diff_diff.json", "r").read())
 diff_set = []
 for i, prime in enumerate(primes):
     if i == 0:
@@ -10,4 +10,4 @@ for i, prime in enumerate(primes):
     diff_set.append(prime_diff)
 plt.plot(range(len(diff_set)), diff_set)
 plt.show()
-open("primes_diff.json", "w").write(json.dumps(diff_set))
+open("primes_diff_diff_diff_diff.json", "w").write(json.dumps(diff_set))
--- solver/iter.py
+++ solver/iter.py
@@ -1,9 +1,15 @@
 
 import json
+import matplotlib.pyplot as plt
+from time import sleep
 from py_expression_eval import Parser
-from random import randint, choice, random
+from random import randint, choice, random, gauss
 
 primes = json.loads(open("../datasets/primes.json", "r").read())
+primes_diff = json.loads(open("../datasets/primes_diff.json", "r").read())
+primes_diff_diff = json.loads(open("../datasets/primes_diff_diff.json", "r").read())
+primes_diff_diff_diff = json.loads(open("../datasets/primes_diff_diff_diff.json", "r").read())
+primes_diff_diff_diff_diff = json.loads(open("../datasets/primes_diff_diff_diff_diff.json", "r").read())
 
 # Current state:
 # https://www.quora.com/Could-you-train-a-machine-learner-to-predict-the-next-prime-number-I-know-there-is-no-pattern-to-PNs-I-am-wondering-if-the-ML-would-figure-it-out
@@ -75,13 +81,15 @@ def make_strand(length):
 
         if last == "g":
             # Glue item.
-            strand.append(choice(["a", "s"]))
+            strand.append(choice(["a", "s", "c"]))
         elif last == "a":
             # Attachment.
             strand.append(choice(["g"]))
         elif last == "s":
             # Substitutable.
             strand.append(choice(["g"]))
+        elif last == "c":
+            strand.append(choice(["a", "s", "g"]))
 
     if strand[-1] == "g":
         strand = strand[0:-1]
@@ -111,6 +119,11 @@ def supplement(strand):
             else:
                 # Append the substitute itself. Nothing bad.
                 given_eq += choice(meta["substitutable"])
+        elif item == "c":
+            cons = gauss(0, 100) # a = base, b = variance
+            # cons = random()*randint(-10000,10000)
+            given_eq += str(cons)
+
         given_eq += " "
     return given_eq
 
@@ -127,12 +140,83 @@ def make_eq():
     # Great.
     response = eval(equation, {"x": 1, "y": 2})
     if response == None:
+        # print(equation)
+        # print()
         return None
     else:
         return equation
 
+best_error = 1000000000
+
+def series_explode(eq):
+    global best_error
+
+    # All series to test.
+    test_series = [
+        primes,
+        primes_diff,
+        primes_diff,
+        primes_diff_diff,
+        primes_diff_diff_diff
+    ]
+
+    # Iterate.
+    for _ in range(4): # 20. Computationally hyper-expensive.
+        for series in test_series: # do random series 10k times
+            # Setting up randoms.
+            iter_by = gauss(0, 10) if random() < 0.5 else randint(-50, 50) # 0.zf9q2809as or -50>50
+            iter_x = 1
+            x = []
+            y = []
+            # Build XY sequence for comparison to series.
+            y_eq_sub = gauss(0, 100)
+            for _ in range(len(series)):
+                x_val = iter_x*iter_by
+                x.append(x_val)
+                y_val = eval(eq, {"x": x_val, "y": y_eq_sub})
+                if y_val == None:
+                    y_val = 0
+                y.append(y_val)
+                iter_x += 1
+            # Compare to series. Use percentage differences.
+            error = 0
+            # error = []
+            for i, created_y_real_y in enumerate(zip(y, series)):
+                if i == 0:
+                    continue
+                # Pack in the last cy, and the cy now. Ry too.
+                cy, ry = created_y_real_y
+                last_cy, last_ry = y[i-1], series[i-1]
+                # Calculate the absolute percentage error.
+                try:
+                    real_cdiff = (cy/last_cy)-1 # Created.
+                    real_rdiff = (ry/last_ry)-1 # Real.
+                    perc_err = abs(real_cdiff - real_rdiff)
+                    # ind_error = abs(cy - ry)
+                    # perc_err =
+                except:
+                    return False
+                # error.append(perc_err)
+                error += perc_err
+            # Check compared.
+            # ape_score = sum(error)/len(error)
+            ape_score = error
+            if ape_score < best_error:
+                best_error = ape_score
+                print(f"> new b/e: {ape_score}. eq: {eq}, iter_by: {iter_by}, y_eq_sub: {y_eq_sub}, series 0-5: {series[0:5]}")
 
 if __name__ == "__main__":
+
+    # eqs = [make_eq() for _ in range(1000)]
+
     while True:
-        eq = make_eq()
-        print(eq)
+        # eq = make_eq()
+        eq = "PI + x * -77.10565555763374"
+        if eq != None:
+            series_explode(eq)
+    # print(eq)
+
+    # print(sum([True for e in eqs if e == None]))
+    #     if eq != None:
+    #         print(eq)
+            # sleep(0.1)
