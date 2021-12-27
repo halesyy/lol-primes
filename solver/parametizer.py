3b70fe7ead5bb7e7ed3ff2f42e5a32ca3b8da987
Jack Hales <halesyy@gmail.com>
2021-12-27 14:21:25 +1100

    pzzr

--- solver/parametizer.py
+++ solver/parametizer.py
@@ -4,6 +4,9 @@ from time import sleep
 from py_expression_eval import Parser
 parser = Parser()
 primes = json.loads(open("../datasets/primes.json", "r").read())
+
+print(">", len(primes), "total primes")
+
 def eval(eq, sub):
     try:
         return parser.parse(eq).evaluate(sub)
@@ -28,7 +31,7 @@ def error(eq, x, y):
     for i, prime in enumerate(primes):
         x_val = iter_x * x
         y_val = eval(eq, {"x": x_val, "y": y})
-        if y_val == None: return 1000000000
+        if y_val == None: return ""
         error = abs(y_val - prime)
         running_error += error
         iter_x += 1
@@ -39,18 +42,44 @@ def error(eq, x, y):
 def make_params(dec_div, iter_range):
     iter_by = 1/dec_div
     params = []
+    x_plot, y_plot = 0, 0
     for x in range(1, (iter_range*dec_div)+1):
-        x_val = x/dec_div
+        x = x/dec_div
         for y in range(0, (iter_range*dec_div)+1):
-            y_val = y/dec_div
-            yield (x_val, y_val, x, y)
+            y = y/dec_div
+            yield (x, y, x_plot, y_plot)
+            y_plot += 1
+        x_plot += 1
 
 p_log = []
 # Iterate, and calculate the error.
-for i, param in enumerate(make_params(10, 10)):
+for i, param in enumerate(make_params(10, 1)):
     x_val, y_val, x, y = param
     err = error(eq, x, y)
-    p_log.append([x_val, y_val, x, y])
+    p_log.append([x_val, y_val, x, y, err])
 
 # Save param log.
 open("reports/params.json", "w").write(json.dumps(p_log, indent=4))
+
+# Convert p_log into the tsv sheet.
+report = []
+report_x = []
+report_y = []
+for row in p_log:
+    x, y, plot_x, plot_y, err = row
+    row_x = plot_x+1
+    if len(report) < row_x:
+        report.append([])
+    report[plot_x].append(str(err))
+    if plot_x == 0:
+        # print("y", y)
+        report_y.append(str(y))
+    if plot_y == 0:
+        report_x.append(str(x))
+report = ["\t".join(r) for r in report]
+report = "\n".join(report)
+report_x = "\t".join(report_x)
+report_y = "\n".join(report_y)
+open("reports/params.tsv", "w").write(report)
+open("reports/params_x.tsv", "w").write(report_x)
+open("reports/params_y.tsv", "w").write(report_y)
