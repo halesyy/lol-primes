fb92028b508ca00aac3bc259d079019e1c9ab12c
Jack Hales <halesyy@gmail.com>
2022-01-08 10:50:02 +1100

    neg

--- datasets/nth_differential/make.py
+++ datasets/nth_differential/make.py
@@ -13,21 +13,26 @@ primes = json.loads(open("../primes_1m_test.json", "r").read())
 
 to_plot = []
 
-for nth_diff in range(500):
+for nth_diff in range(1000):
     nth_diff = nth_diff + 1
     # primes = primes[0:nth_diff]
     working_diff = [p for p in primes[0:nth_diff]]
     # print(nth_diff, len(working_diff))
     for i in range(nth_diff):
-        if i == nth_diff-2:
+        if i >= nth_diff-5:
             print(f"> First 10 diffs at n={i}: {list(working_diff[0:10])}")
         working_diff = numpy.diff(working_diff)
         working_diff = [abs(p) for p in working_diff]
         to_plot.append([range(len(working_diff)), working_diff])
+    print()
 
 # Non-up curves differentiate into +infinity as they are given
 # nth diff complexities, whereas a positive/basic curve's diffs
 # will create a static line (y=x), or devolve to 0 (y=log(x)).
+
+# Big difference between absolute differencing, and normative
+# differencing, I'm guessing there's also a random negative flip
+# as well in this iteration.
 
 # to_plot.reverse()
 # for x, y in to_plot:
